5db19be88ce0556c0882385041aa587530a904dd
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2024-02-08 02:47:02 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-server-production-21e1.up.railway.app']
+ALLOWED_HOSTS = ['django-server-production-b2c6.up.railway.app']
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ['https://django-server-production-21e1.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-b2c6.up.railway.app']
 
 # Application definition
 
